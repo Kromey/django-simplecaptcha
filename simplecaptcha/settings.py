d56a48c1b217aa6efa253a341ef9ca9452718d5a
Travis Veazey <travisvz@gmail.com>
2014-12-22 00:13:50 -0600

    Refactor settings module to be DRY
    Settings module now uses a function to fetch a setting value, falling back to a supplied default

--- simplecaptcha/settings.py
+++ simplecaptcha/settings.py
@@ -1,16 +1,25 @@
 from django.conf import settings
 
 
+def getsetting(setting, default):
+    """Get `setting` if set, fallback to `default` if not
+
+    This method tries to return the value of the specified setting from Django's
+    settings module. If this fails for any reason, the value supplied in
+    `default` will be returned instead.
+    """
+    try:
+        return getattr(settings, setting)
+    except:
+        return default
+
+
 """SIMPLECAPTCHA_DURATION defines how long, in seconds, a captcha is valid for
 
 This setting can either be supplied in your Django project's settings.py file,
 or left out; in the latter case, the default of 5 minutes will be used.
 """
-try:
-    SIMPLECAPTCHA_DURATION = settings.SIMPLECAPTCHA_DURATION
-except:
-    # Valid for 5 minutes by default
-    SIMPLECAPTCHA_DURATION = 60 * 5
+SIMPLECAPTCHA_DURATION = getsetting('SIMPLECAPTCHA_DURATION', 5 * 60)
 
 
 """SIMPLECAPTCHA_ITERATIONS defines how many hashing iterations are performed
@@ -18,8 +27,5 @@ except:
 This can be set in Django's settings module; if left unset, it will default
 to using 1024 iterations.
 """
-try:
-    SIMPLECAPTCHA_ITERATIONS = settings.SIMPLECAPTCHA_ITERATIONS
-except:
-    SIMPLECAPTCHA_ITERATIONS = 1024
+SIMPLECAPTCHA_ITERATIONS = getsetting('SIMPLECAPTCHA_ITERATIONS', 1024)
 
